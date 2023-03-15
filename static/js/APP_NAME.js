6f25fdb330f85b74d75868f8d1ff647507c98eae
Luke Branson <brans109@d.umn.edu>
2023-03-15 10:27:54 -0500

    Red Flag questions answered. TODO: Handle Provider scheduling stuff.

--- static/js/APP_NAME.js
+++ static/js/APP_NAME.js
@@ -1,1 +1,8 @@
-// Put Javascript Here
+function hide_or_show_div(div) {
+  var x = document.getElementById(div);
+  if (x.style.display === "none") {
+    x.style.display = "inline-block";
+  } else {
+    x.style.display = "none";
+  }
+}
