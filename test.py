f89ab6b12859ab51ff5a399a0c1051f1d8b0f1d5
PavanBhat17 <135166725+PavanBhat17@users.noreply.github.com>
2023-06-17 01:03:54 +0530

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-# program to show how the module can be imported. 
-from prediction import predict
- 
-symptoms = ['Symptom_1', 'Symptom_2', 'Symptom_3']    # insert symptoms from dataset
-
-# predicting the disease
-predict(symptoms)
